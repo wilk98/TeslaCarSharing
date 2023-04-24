a1fa3d7bcfb4da407b8935aec41a45c057869043
wilk98 <maciekwilk09@gmail.com>
2023-04-24 21:14:13 +0200

    create TeslaCarSharing context and install EntityFrameworkCore

---
+++ src/infrastructure/TeslaCarSharing.Infrastructure/TeslaCarSharingContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Infrastructure;
+
+public class TeslaCarSharingContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+
+    public TeslaCarSharingContext(DbContextOptions<TeslaCarSharingContext> options) : base(options)
+    {      
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(message => Debug.WriteLine(message));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
