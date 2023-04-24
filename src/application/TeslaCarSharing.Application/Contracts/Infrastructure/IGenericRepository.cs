8188329addcfd15f34b1fa71f788268f9362b063
wilk98 <maciekwilk09@gmail.com>
2023-04-24 21:20:23 +0200

    create interface of GenericRepository in application layer

---
+++ src/application/TeslaCarSharing.Application/Contracts/Infrastructure/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeslaCarSharing.Application.Contracts.Infrastructure;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> Get(int id);
+    Task<IReadOnlyList<T>> GetAll();
+    Task<T> Add(T entity);
+    Task Update(T entity);
+    Task Delete(T entity);
+    Task<bool> Exists(int id);
+}
