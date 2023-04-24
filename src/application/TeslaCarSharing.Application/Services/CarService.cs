eecc8fc73f5b92efd4dc6256784320cc84a95093
wilk98 <maciekwilk09@gmail.com>
2023-04-25 01:24:14 +0200

    create CarService

---
+++ src/application/TeslaCarSharing.Application/Services/CarService.cs
@@ -0,0 +1,15 @@
+﻿using TeslaCarSharing.Application.Contracts.Application;
+using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Application.Services;
+
+public class CarService : GenericService<Car>, ICarService
+{
+    private readonly ICarRepository _repository;
+
+    public CarService(ICarRepository repository) : base(repository)
+    {
+        _repository = repository;
+    }
+}
