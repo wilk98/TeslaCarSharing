4f8d6fc7332035f3d6628aec0e89ddb5b94229b1
wilk98 <maciekwilk09@gmail.com>
2023-04-24 22:51:01 +0200

    create CustomerService

---
+++ src/application/TeslaCarSharing.Application/Services/CustomerService.cs
@@ -0,0 +1,14 @@
+﻿using TeslaCarSharing.Application.Contracts.Application;
+using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Application.Services;
+public class CustomerService : GenericService<Customer>, ICustomerService
+{
+    private readonly ICustomerRepository _repository;
+
+    public CustomerService(ICustomerRepository repository) : base(repository)
+    {
+        _repository = repository;
+    }
+}
