fe2687cebd0c1eca0b96aee9383f12afa15f1341
wilk98 <maciekwilk09@gmail.com>
2023-04-25 00:43:54 +0200

    create Car class

---
+++ src/core/TeslaCarSharing.Core/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace TeslaCarSharing.Core;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string RegistrationNumber { get; set; }
+    public TeslaModel Model { get; set; }
+    public string Color { get; set; }
+    public int Year { get; set; }
+    public bool IsAvailable { get; set; }
+    public Location Location { get; set; }
+}
+
