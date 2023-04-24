cdd716390ea722db70eb67d6ee4c5a350f59791c
wilk98 <maciekwilk09@gmail.com>
2023-04-25 01:20:56 +0200

    create CarRepository

---
+++ src/infrastructure/TeslaCarSharing.Infrastructure/Repositories/CarRepository.cs
@@ -0,0 +1,12 @@
+﻿using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Infrastructure.Repositories;
+
+public class CarRepository : GenericRepository<Car>, ICarRepository
+{
+    public CarRepository(TeslaCarSharingDbContext context) : base(context)
+    {
+    }
+
+}
