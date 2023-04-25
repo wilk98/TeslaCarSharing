851d8dc58972f1f4582ef6aceb06f385c6621fa0
wilk98 <maciekwilk09@gmail.com>
2023-04-25 20:46:29 +0200

    add services and repositories(Car and Customer) and AutoMapper to Program.cs

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,20 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using AutoMapper;
+using TeslaCarSharing.Application.Contracts.Application;
+using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Application.Services;
+using TeslaCarSharing.Infrastructure;
+using TeslaCarSharing.Infrastructure.Repositories;
+using TeslaCarSharing.Application.Profiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddDbContext<TeslaCarSharingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TeslaCarSharingConnectionString"));
+});
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<ICarService, CarService>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
