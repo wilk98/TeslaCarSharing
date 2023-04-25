db5d999c12e9a275b1810ec6cac9fd638dde56b9
wilk98 <maciekwilk09@gmail.com>
2023-04-25 20:45:32 +0200

    create MappingProfile for Customer/CustomerDto and Car/CarDto

---
+++ src/application/TeslaCarSharing.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using TeslaCarSharing.Application.DTOs.Customer;
+using TeslaCarSharing.Core;
+using AutoMapper;
+using TeslaCarSharing.Application.DTOs.Car;
+
+namespace TeslaCarSharing.Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Customer, CustomerDto>();
+        CreateMap<Car, CarDto>();
+    }
+}
