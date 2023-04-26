bb7986a2a6bff2044ff0e18477aab85319765cc0
wilk98 <maciekwilk09@gmail.com>
2023-04-26 16:44:15 +0200

    create navbar component with logo and buttons with links to change page

---
+++ src/api/ClientApp/tesla-car-sharing/src/layout/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import Logo from "../Logo/Logo";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <ul className="nav_container">
+      <li>
+        <Link to="/about" className="nav_button">About us</Link>
+      </li>
+      <li>
+        <Link to="/cars" className="nav_button">Our cars</Link>
+      </li>
+      <li>
+        <Link to="/reservation" className="nav_button">Reservation</Link>
+      </li>
+      <li>
+        <Link to="/price-list" className="nav_button">Price list</Link>
+      </li>
+      <li>
+        <Link to="/contact" className="nav_button">Contact</Link>
+      </li>
+    </ul>
+  );
+};
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <Menu />
+    </nav>
+  );
+};
