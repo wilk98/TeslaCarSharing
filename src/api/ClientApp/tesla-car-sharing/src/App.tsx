9e82ec3371e16be828f19ea852406c1050eeeeac
wilk98 <maciekwilk09@gmail.com>
2023-04-26 16:48:51 +0200

    clean default App.tsx, add import pages, Navbar and Footer to App.tsx, add Route to pages

--- src/api/ClientApp/tesla-car-sharing/src/App.tsx
+++ src/api/ClientApp/tesla-car-sharing/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './layout/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Cars from './pages/Cars/Cars';
+import Reservation from './pages/Reservation/Reservation';
+import PriceList from './pages/PriceList/PriceList';
+import Contact from './pages/Contact/Contact';
+import Footer from './layout/Footer/Footer';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/cars" element={<Cars/>} />
+        <Route path="/reservation" element={<Reservation/>} />
+        <Route path="/price-list" element={<PriceList/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
