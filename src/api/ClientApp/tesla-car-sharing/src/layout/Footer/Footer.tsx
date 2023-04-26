035bbd9f2104388d062a97ad73b0c9ee08be7552
wilk98 <maciekwilk09@gmail.com>
2023-04-26 16:45:01 +0200

    create basic footer

---
+++ src/api/ClientApp/tesla-car-sharing/src/layout/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer_container">
+      <div className="footer_left">
+        <p>© 2023 Mallorca Tesla Rental Company</p>
+      </div>
+      <div className="footer_right">
+        <a href="#">Terms and Conditions</a>
+        <a href="#">Privacy Policy</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
