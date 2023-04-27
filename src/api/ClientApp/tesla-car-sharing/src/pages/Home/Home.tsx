9aa33e0b53f562b169a38ef4240c9d7010a175f9
wilk98 <maciekwilk09@gmail.com>
2023-04-27 19:46:22 +0200

    create Home page

--- src/api/ClientApp/tesla-car-sharing/src/pages/Home/Home.tsx
+++ src/api/ClientApp/tesla-car-sharing/src/pages/Home/Home.tsx
@@ -1,10 +1,64 @@
-const Home = () => {
-    return (
-      <div>
-        <h1>Home</h1>
+import React from "react";
+import { Link } from "react-router-dom";
+import HomeImage from "./Images/HomeImage.png"
+import TeslaS from "./Images/TeslaS.png";
+import Tesla3 from "./Images/Tesla3.png";
+import TeslaX from "./Images/TeslaX.png";
+import TeslaY from "./Images/TeslaY.png";
+import "./Home.css";
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <div className="hero">
+        <img src={HomeImage} alt="Tesla" className="hero-image" /> 
+        <div className="hero-text">
+          <h1>Rent a Tesla in Mallorca</h1>
+          <p>Experience the future of driving today.</p>
+          <Link to="/reservation" className="cta">
+            Book Now
+          </Link>
+        </div>
       </div>
-    );
-  };
-  
-  export default Home;
-  
+      <div className="models">
+        <div className="model">
+          <img src={TeslaS} alt="Tesla Model S" />
+          <h2>Tesla Model S</h2>
+          <p>
+            The Model S combines performance, safety, and efficiency like no
+            other sedan. With its long range and advanced features, it’s the
+            ultimate electric luxury car.
+          </p>
+        </div>
+        <div className="model">
+          <img src={Tesla3} alt="Tesla Model 3" />
+          <h2>Tesla Model 3</h2>
+          <p>
+            The Model 3 is designed for electric-powered performance, with a
+            sleek, modern design that conveys speed and agility.
+          </p>
+        </div>
+        <div className="model">
+          <img src={TeslaX} alt="Tesla Model X" />
+          <h2>Tesla Model X</h2>
+          <p>
+            The Model X combines the space and functionality of a sport utility
+            vehicle with the uncompromised performance of a Tesla. With its
+            Falcon Wing doors, it’s the ultimate family car.
+          </p>
+        </div>
+        <div className="model">
+          <img src={TeslaY} alt="Tesla Model Y" />
+          <h2>Tesla Model Y</h2>
+          <p>
+            The Model Y is a mid-size SUV that’s both practical and fun to
+            drive. With its long range and spacious interior, it’s the perfect
+            vehicle for exploring Mallorca.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
