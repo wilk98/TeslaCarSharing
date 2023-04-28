a023fe352b640d5e4c217a514d2f6e25c1a7685b
wilk98 <maciekwilk09@gmail.com>
2023-04-28 20:59:40 +0200

    create PriceList page with table (cars and prices)

--- src/api/ClientApp/tesla-car-sharing/src/pages/PriceList/PriceList.tsx
+++ src/api/ClientApp/tesla-car-sharing/src/pages/PriceList/PriceList.tsx
@@ -1,10 +1,61 @@
-const PriceList = () => {
-    return (
-      <div>
-        <h1>Price List</h1>
+// PriceList.tsx
+
+import "./PriceList.css";
+import teslaXImg from "../../assets/TeslaX.png";
+import teslaYImg from "../../assets/TeslaY.png";
+import teslaSImg from "../../assets/TeslaS.png";
+import tesla3Img from "../../assets/Tesla3.png";
+
+const carList = [
+  {
+    name: "Tesla Model X",
+    image: teslaXImg,
+    price: 250,
+  },
+  {
+    name: "Tesla Model Y",
+    image: teslaYImg,
+    price: 180,
+  },
+  {
+    name: "Tesla Model S",
+    image: teslaSImg,
+    price: 200,
+  },
+  {
+    name: "Tesla Model 3",
+    image: tesla3Img,
+    price: 150,
+  },
+];
+
+const PriceList: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Price List</h1>
+      <div className="price-list-container">
+        <table className="price-list">
+          <thead>
+            <tr>
+              <th>Car</th>
+              <th>Price per day (EUR)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {carList.map((car) => (
+              <tr key={car.name}>
+                <td>
+                  <img src={car.image} alt={car.name} />
+                  <h3>{car.name}</h3>
+                </td>
+                <td>{car.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  };
-  
-  export default PriceList;
-  
+    </div>
+  );
+};
+
+export default PriceList;
