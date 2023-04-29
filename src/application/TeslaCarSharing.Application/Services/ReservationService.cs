c79fd992793d70cfff3f2d64633961b3a4266329
wilk98 <maciekwilk09@gmail.com>
2023-04-29 02:24:12 +0200

    create ReservationService class

---
+++ src/application/TeslaCarSharing.Application/Services/ReservationService.cs
@@ -0,0 +1,14 @@
+﻿using TeslaCarSharing.Application.Contracts.Application;
+using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Application.Services;
+
+public class ReservationService : GenericService<Reservation>, IReservationService
+{
+    private readonly IReservationRepository _repository;
+    public ReservationService(IReservationRepository repository) : base(repository)
+    {
+        _repository = repository;
+    }
+}
