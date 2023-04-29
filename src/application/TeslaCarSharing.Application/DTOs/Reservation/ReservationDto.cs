1b5a1973d7c41c6e8cb55d7aabf7a1168755ea54
wilk98 <maciekwilk09@gmail.com>
2023-04-29 02:13:24 +0200

    add ReservationDto class

---
+++ src/application/TeslaCarSharing.Application/DTOs/Reservation/ReservationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TeslaCarSharing.Application.DTOs.Reservation;
+
+public class ReservationDto
+{
+    public int Id { get; set; }
+    public int CarId { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal TotalPrice { get; set; }
+}
