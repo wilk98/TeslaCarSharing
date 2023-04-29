982c1ef54a9ea779d8c8c045efa15f64e6ea29fc
wilk98 <maciekwilk09@gmail.com>
2023-04-29 02:35:50 +0200

    create Reservation Controller with get, get all, delete, add and update

--- src/api/Contollers/CustomerController.cs
+++ src/api/Contollers/CustomerController.cs
@@ -6,74 +6,73 @@ using TeslaCarSharing.Application.DTOs.Customer;
 using TeslaCarSharing.Core;
 using AutoMapper;
 
-namespace TeslaCarSharing.Api.Controllers
+namespace TeslaCarSharing.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CustomerController : ControllerBase
+    private readonly ICustomerService _customerService;
+    private readonly IMapper _mapper;
+
+    public CustomerController(ICustomerService customerService, IMapper mapper)
     {
-        private readonly ICustomerService _customerService;
-        private readonly IMapper _mapper;
+        _customerService = customerService;
+        _mapper = mapper;
+    }
 
-        public CustomerController(ICustomerService customerService, IMapper mapper)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CustomerDto>> GetCustomer(int id)
+    {
+        var customer = await _customerService.Get(id);
+        if (customer == null)
         {
-            _customerService = customerService;
-            _mapper = mapper;
+            return NotFound();
+        }
+        var customerDto = _mapper.Map<CustomerDto>(customer);
+        return Ok(customerDto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CustomerDto>>> GetCustomers()
+    {
+        var customers = await _customerService.GetAll();
+        var customersDto = _mapper.Map<IEnumerable<CustomerDto>>(customers);
+        return Ok(customersDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerDto>> AddCustomer(Customer customer)
+    {
+        var addedCustomer = await _customerService.Add(customer);
+        var customerDto = _mapper.Map<CustomerDto>(addedCustomer);
+        return CreatedAtAction(nameof(GetCustomer), new { id = customerDto.Id }, customerDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+    {
+        if (id != customer.Id)
+        {
+            return BadRequest();
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<CustomerDto>> GetCustomer(int id)
+        await _customerService.Update(customer);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCustomer(int id)
+    {
+        var customer = await _customerService.Get(id);
+        if (customer == null)
         {
-            var customer = await _customerService.Get(id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-            var customerDto = _mapper.Map<CustomerDto>(customer);
-            return Ok(customerDto);
+            return NotFound();
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CustomerDto>>> GetCustomers()
-        {
-            var customers = await _customerService.GetAll();
-            var customersDto = _mapper.Map<IEnumerable<CustomerDto>>(customers);
-            return Ok(customersDto);
-        }
+        await _customerService.Delete(customer);
 
-        [HttpPost]
-        public async Task<ActionResult<CustomerDto>> AddCustomer(Customer customer)
-        {
-            var addedCustomer = await _customerService.Add(customer);
-            var customerDto = _mapper.Map<CustomerDto>(addedCustomer);
-            return CreatedAtAction(nameof(GetCustomer), new { id = customerDto.Id }, customerDto);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
-        {
-            if (id != customer.Id)
-            {
-                return BadRequest();
-            }
-
-            await _customerService.Update(customer);
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCustomer(int id)
-        {
-            var customer = await _customerService.Get(id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            await _customerService.Delete(customer);
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
---
+++ src/api/Contollers/ReservationController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TeslaCarSharing.Application.Contracts.Application;
+using TeslaCarSharing.Application.DTOs.Customer;
+using TeslaCarSharing.Application.DTOs.Reservation;
+using TeslaCarSharing.Application.Services;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Api.Contollers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReservationController : ControllerBase
+{
+    private readonly IReservationService _reservationService;
+    private readonly IMapper _mapper;
+
+    public ReservationController(IReservationService reservationService, IMapper mapper)
+    {
+        _reservationService = reservationService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ReservationDto>> GetReservation(int id)
+    {
+        var reservation = await _reservationService.Get(id);
+        if (reservation == null)
+        {
+            return NotFound();
+        }
+        var reservationDto = _mapper.Map<ReservationDto>(reservation);
+        return Ok(reservationDto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ReservationDto>>> GetReservations()
+    {
+        var reservations = await _reservationService.GetAll();
+        var reservationsDto = _mapper.Map<IEnumerable<ReservationDto>>(reservations);
+        return Ok(reservationsDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ReservationDto>> AddReservation(Reservation reservation)
+    {
+        var addedReservation = await _reservationService.Add(reservation);
+        var reservationDto = _mapper.Map<ReservationDto>(addedReservation);
+        return CreatedAtAction(nameof(GetReservation), new { id = reservationDto.Id }, reservationDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateReservation(int id, Reservation reservation)
+    {
+        if (id != reservation.Id)
+        {
+            return BadRequest();
+        }
+
+        await _reservationService.Update(reservation);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReservation(int id)
+    {
+        var reservation = await _reservationService.Get(id);
+        if (reservation == null)
+        {
+            return NotFound();
+        }
+
+        await _reservationService.Delete(reservation);
+
+        return NoContent();
+    }
+}
