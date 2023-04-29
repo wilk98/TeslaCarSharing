58d3a6712f9bd53ccfa58e779e90cf71dcc098ef
wilk98 <maciekwilk09@gmail.com>
2023-04-29 02:09:25 +0200

    add reservation class

---
+++ src/core/TeslaCarSharing.Core/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace TeslaCarSharing.Core;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public int CarId { get; set; }
+    public Car Car { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal TotalPrice { get; set; }
+
+
+}
