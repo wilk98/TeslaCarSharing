dff8cdf2d1c1ff01cd98d54b28d56ce02c476633
wilk98 <maciekwilk09@gmail.com>
2023-04-29 02:18:25 +0200

    create Reservation Repository class

---
+++ src/infrastructure/TeslaCarSharing.Infrastructure/Repositories/ReservationRepository.cs
@@ -0,0 +1,11 @@
+﻿using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Infrastructure.Repositories;
+
+public class ReservationRepository : GenericRepository<Reservation>, IReservationRepository
+{
+    public ReservationRepository(TeslaCarSharingDbContext context) : base(context)
+    {
+    }
+}
