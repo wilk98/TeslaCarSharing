f8b3d79a8f791139caa5fa71ff888a8aa810893e
wilk98 <maciekwilk09@gmail.com>
2023-05-01 13:57:55 +0200

    delete the same part of code after merge

--- src/api/Contollers/CustomerController.cs
+++ src/api/Contollers/CustomerController.cs
@@ -12,67 +12,61 @@ namespace TeslaCarSharing.Api.Controllers;
 public class CustomerController : ControllerBase
 {
     private readonly ICustomerService _customerService;
-    private readonly IMapper _mapper;
 
-    public CustomerController(ICustomerService customerService, IMapper mapper)
+    public CustomerController(ICustomerService customerService)
     {
-        private readonly ICustomerService _customerService;
+        _customerService = customerService;
+    }
 
-        public CustomerController(ICustomerService customerService)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Customer>> GetCustomer(int id)
+    {
+        var customer = await _customerService.Get(id);
+        if (customer == null)
         {
-            _customerService = customerService;
+            return NotFound();
+        }
+        return Ok(customer);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+    {
+        var customers = await _customerService.GetAll();
+        return Ok(customers);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Customer>> AddCustomer(Customer customer)
+    {
+        var addedCustomer = await _customerService.Add(customer);
+        return CreatedAtAction(nameof(GetCustomer), new { id = addedCustomer.Id }, addedCustomer);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+    {
+        if (id != customer.Id)
+        {
+            return BadRequest();
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        await _customerService.Update(customer);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCustomer(int id)
+    {
+        var customer = await _customerService.Get(id);
+        if (customer == null)
         {
-            var customer = await _customerService.Get(id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-            return Ok(customer);
+            return NotFound();
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
-        {
-            var customers = await _customerService.GetAll();
-            return Ok(customers);
-        }
+        await _customerService.Delete(customer);
 
-        [HttpPost]
-        public async Task<ActionResult<Customer>> AddCustomer(Customer customer)
-        {
-            var addedCustomer = await _customerService.Add(customer);
-            return CreatedAtAction(nameof(GetCustomer), new { id = addedCustomer.Id }, addedCustomer);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
-        {
-            if (id != customer.Id)
-            {
-                return BadRequest();
-            }
-
-            await _customerService.Update(customer);
-
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCustomer(int id)
-        {
-            var customer = await _customerService.Get(id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            await _customerService.Delete(customer);
-
-            return NoContent();
-        }
+        return NoContent();
     }
 }
