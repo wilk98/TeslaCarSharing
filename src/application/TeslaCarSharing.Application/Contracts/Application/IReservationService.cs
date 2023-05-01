95c678bedcfbeb11e553799e934148dc818bf776
wilk98 <maciekwilk09@gmail.com>
2023-05-01 18:27:30 +0200

    remove GenericService from interface of ReservationService and add methods to add and get

--- src/application/TeslaCarSharing.Application/Contracts/Application/IReservationService.cs
+++ src/application/TeslaCarSharing.Application/Contracts/Application/IReservationService.cs
@@ -1,7 +1,14 @@
-﻿using TeslaCarSharing.Core;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TeslaCarSharing.Application.DTOs.Reservation;
+using TeslaCarSharing.Core;
 
 namespace TeslaCarSharing.Application.Contracts.Application;
 
-public interface IReservationService : IGenericService<Reservation>
+public interface IReservationService
 {
+    Task<int> AddReservationAsync(CreateReservationDto reservationDto);
+    Task<ReservationDto> GetReservationAsync(int reservationId);
+    Task<IReadOnlyList<ReservationDto>> GetAllReservationsAsync();
+    Task<IReadOnlyList<int>> GetUnavailableCarIdsAsync(DateTime startDate, DateTime endDate);
 }
