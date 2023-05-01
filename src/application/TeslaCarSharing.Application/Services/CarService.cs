c17fcf50d61e55f12e153122a870ecf9ca4156b7
wilk98 <maciekwilk09@gmail.com>
2023-05-01 18:18:07 +0200

    add GetAvailableCars to CarService

--- src/application/TeslaCarSharing.Application/Services/CarService.cs
+++ src/application/TeslaCarSharing.Application/Services/CarService.cs
@@ -1,4 +1,5 @@
-﻿using TeslaCarSharing.Application.Contracts.Application;
+﻿using System.Linq;
+using TeslaCarSharing.Application.Contracts.Application;
 using TeslaCarSharing.Application.Contracts.Infrastructure;
 using TeslaCarSharing.Core;
 
@@ -6,10 +7,21 @@ namespace TeslaCarSharing.Application.Services;
 
 public class CarService : GenericService<Car>, ICarService
 {
-    private readonly ICarRepository _repository;
+    private readonly ICarRepository _carRepository;
+    private readonly IReservationService _reservationService;
 
-    public CarService(ICarRepository repository) : base(repository)
+    public CarService(ICarRepository carRepository, IReservationService reservationService) : base(carRepository)
     {
-        _repository = repository;
+        _carRepository = carRepository;
+        _reservationService = reservationService;
+    }
+    public async Task<IReadOnlyList<Car>> GetAvailableCarsAsync(DateTime startDate, DateTime endDate)
+    {
+        var cars = await _carRepository.GetAll();
+        var unavailableCarIds = await _reservationService.GetUnavailableCarIdsAsync(startDate, endDate);
+
+        var availableCars = cars.Where(c => !unavailableCarIds.Contains(c.Id));
+
+        return availableCars.ToList();
     }
 }
