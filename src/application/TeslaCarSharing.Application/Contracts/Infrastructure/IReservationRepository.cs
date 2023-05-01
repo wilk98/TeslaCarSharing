8bc345dbd3f61a89c3054e1a1d8d1085a8d98967
wilk98 <maciekwilk09@gmail.com>
2023-05-01 18:24:37 +0200

    add to interface ReservationRepository GetUnavailableCarIds

--- src/application/TeslaCarSharing.Application/Contracts/Infrastructure/IReservationRepository.cs
+++ src/application/TeslaCarSharing.Application/Contracts/Infrastructure/IReservationRepository.cs
@@ -4,4 +4,6 @@ namespace TeslaCarSharing.Application.Contracts.Infrastructure;
 
 public interface IReservationRepository : IGenericRepository<Reservation>
 {
+    Task<IReadOnlyList<int>> GetUnavailableCarIdsAsync(DateTime startDate, DateTime endDate);
 }
+
