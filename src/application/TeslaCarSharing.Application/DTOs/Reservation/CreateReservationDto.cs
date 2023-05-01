444be46c7b0961e4a4353413a8e35cdfa716592e
wilk98 <maciekwilk09@gmail.com>
2023-05-01 18:19:45 +0200

    add CreateReservationDto

---
+++ src/application/TeslaCarSharing.Application/DTOs/Reservation/CreateReservationDto.cs
@@ -0,0 +1,14 @@
+﻿using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Application.DTOs.Reservation;
+
+public class CreateReservationDto
+{
+    public int Id { get; set; }
+    public int CarId { get; set; }
+    public Customer Customer { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public Location StartLocation { get; set; }
+    public Location EndLocation { get; set; }
+}
