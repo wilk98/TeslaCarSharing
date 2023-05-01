c35ca076ab4f125abbea5ae6c6fd79cec48848d8
wilk98 <maciekwilk09@gmail.com>
2023-05-01 18:25:05 +0200

    add to ReservationRepository GetUnavailableCarIds

--- src/infrastructure/TeslaCarSharing.Infrastructure/Repositories/ReservationRepository.cs
+++ src/infrastructure/TeslaCarSharing.Infrastructure/Repositories/ReservationRepository.cs
@@ -1,11 +1,27 @@
-﻿using TeslaCarSharing.Application.Contracts.Infrastructure;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TeslaCarSharing.Application.Contracts.Infrastructure;
+using TeslaCarSharing.Application.DTOs.Reservation;
 using TeslaCarSharing.Core;
 
 namespace TeslaCarSharing.Infrastructure.Repositories;
 
 public class ReservationRepository : GenericRepository<Reservation>, IReservationRepository
 {
+    private readonly TeslaCarSharingDbContext _context;
     public ReservationRepository(TeslaCarSharingDbContext context) : base(context)
     {
+        _context = context;
+    }
+    public async Task<IReadOnlyList<int>> GetUnavailableCarIdsAsync(DateTime startDate, DateTime endDate)
+    {
+        var reservations = await _context.Reservations
+            .Where(r => (r.StartDate <= startDate && r.EndDate >= startDate) ||
+                        (r.StartDate >= startDate && r.StartDate <= endDate))
+            .ToListAsync();
+
+        var carIds = reservations.Select(r => r.CarId).Distinct().ToList();
+
+        return carIds;
     }
 }
