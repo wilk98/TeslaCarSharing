5515101f2c5603c649b8f5df3a2e6096fee91282
wilk98 <maciekwilk09@gmail.com>
2023-05-02 20:13:19 +0200

    change Customer to CustomerDto in Customer Controller

--- src/api/Contollers/CustomerController.cs
+++ src/api/Contollers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using TeslaCarSharing.Application.Contracts.Application;
 using TeslaCarSharing.Core;
 using AutoMapper;
+using TeslaCarSharing.Application.DTOs.Customer;
 
 namespace TeslaCarSharing.Api.Controllers;
 
@@ -19,9 +20,9 @@ public class CustomerController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Customer>> GetCustomer(int id)
+    public async Task<ActionResult<CustomerDto>> GetCustomer(int id)
     {
-        var customer = await _customerService.Get(id);
+        var customer = await _customerService.GetCustomerAsync(id);
         if (customer == null)
         {
             return NotFound();
@@ -30,21 +31,21 @@ public class CustomerController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+    public async Task<ActionResult<IEnumerable<CustomerDto>>> GetCustomers()
     {
-        var customers = await _customerService.GetAll();
+        var customers = await _customerService.GetAllCustomersAsync();
         return Ok(customers);
     }
 
     [HttpPost]
-    public async Task<ActionResult<Customer>> AddCustomer(Customer customer)
+    public async Task<ActionResult<CustomerDto>> AddCustomer(CustomerDto customer)
     {
         var addedCustomer = await _customerService.Add(customer);
         return CreatedAtAction(nameof(GetCustomer), new { id = addedCustomer.Id }, addedCustomer);
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCustomer(int id, Customer customer)
+    public async Task<IActionResult> UpdateCustomer(int id, CustomerDto customer)
     {
         if (id != customer.Id)
         {
@@ -59,13 +60,13 @@ public class CustomerController : ControllerBase
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteCustomer(int id)
     {
-        var customer = await _customerService.Get(id);
+        var customer = await _customerService.GetCustomerAsync(id);
         if (customer == null)
         {
             return NotFound();
         }
 
-        await _customerService.Delete(customer);
+        await _customerService.Delete(customer.Id);
 
         return NoContent();
     }
