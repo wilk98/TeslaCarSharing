9ad271c5cfc35d3f74a7a1c26f5551807f008a39
wilk98 <maciekwilk09@gmail.com>
2023-05-02 20:09:40 +0200

    add CarDto

---
+++ src/application/TeslaCarSharing.Application/DTOs/Car/CarDto.cs
@@ -0,0 +1,15 @@
+﻿using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Application.DTOs.Car;
+
+public class CarDto
+{
+    public int Id { get; set; }
+    public string RegistrationNumber { get; set; }
+    public TeslaModel Model { get; set; }
+    public string Color { get; set; }
+    public int Year { get; set; }
+    public Location Location { get; set; }
+    public decimal PricePerDay { get; set; }
+
+}
