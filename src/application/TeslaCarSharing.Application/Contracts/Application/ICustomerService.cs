fbb981217f29788e115311a4f995aa6f0f97a0a0
wilk98 <maciekwilk09@gmail.com>
2023-05-02 20:11:14 +0200

    add tasks to interface of Customer Service

--- src/application/TeslaCarSharing.Application/Contracts/Application/ICustomerService.cs
+++ src/application/TeslaCarSharing.Application/Contracts/Application/ICustomerService.cs
@@ -1,7 +1,12 @@
-﻿using TeslaCarSharing.Core;
+﻿using TeslaCarSharing.Application.DTOs.Customer;
 
 namespace TeslaCarSharing.Application.Contracts.Application;
 
-public interface ICustomerService : IGenericService<Customer>
+public interface ICustomerService
 {
+    Task<CustomerDto> GetCustomerAsync(int customerId);
+    Task<IReadOnlyList<CustomerDto>> GetAllCustomersAsync();
+    Task<CustomerDto> Add(CustomerDto customerDto);
+    Task Update(CustomerDto customerDto);
+    Task Delete(int customerId);
 }
