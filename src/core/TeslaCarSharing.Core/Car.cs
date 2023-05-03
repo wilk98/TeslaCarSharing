049e850f1f7c470ed6df073876e525df4b81ddfd
wilk98 <maciekwilk09@gmail.com>
2023-05-03 19:37:44 +0200

    delete switch from Car class

--- src/core/TeslaCarSharing.Core/Car.cs
+++ src/core/TeslaCarSharing.Core/Car.cs
@@ -8,24 +8,6 @@ public class Car
     public string Color { get; set; }
     public int Year { get; set; }
     public Location Location { get; set; }
-    public decimal PricePerDay
-    {
-        get
-        {
-            switch (Model)
-            {
-                case TeslaModel.ModelS:
-                    return CarPrices.ModelSPricePerDay;
-                case TeslaModel.Model3:
-                    return CarPrices.Model3PricePerDay;
-                case TeslaModel.ModelX:
-                    return CarPrices.ModelXPricePerDay;
-                case TeslaModel.ModelY:
-                    return CarPrices.ModelYPricePerDay;
-                default:
-                    throw new Exception("Invalid Tesla model");
-            }
-        }
-    }
+    public decimal PricePerDay { get; set; }
 }
 
