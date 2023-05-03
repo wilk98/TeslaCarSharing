fc1d4d9abafa70ef2b9c28ebd4355be612be8b0a
wilk98 <maciekwilk09@gmail.com>
2023-05-03 19:38:40 +0200

    add TeslaPricePerDayProvider class

---
+++ src/application/TeslaCarSharing.Application/Services/TeslaPricePerDayProvider.cs
@@ -0,0 +1,24 @@
+﻿using TeslaCarSharing.Application.Contracts.Application;
+using TeslaCarSharing.Core;
+
+namespace TeslaCarSharing.Application.Services;
+
+public class TeslaPricePerDayProvider : IPricePerDayProvider
+{
+    public decimal GetPricePerDay(TeslaModel model)
+    {
+        switch (model)
+        {
+            case TeslaModel.ModelS:
+                return 200;
+            case TeslaModel.Model3:
+                return 150;
+            case TeslaModel.ModelX:
+                return 250;
+            case TeslaModel.ModelY:
+                return 180;
+            default:
+                throw new Exception("Invalid Tesla model");
+        }
+    }
+}
