7b0ec234d1230e8cdc9607aed18e67b4df78e76d
wilk98 <maciekwilk09@gmail.com>
2023-05-03 17:45:10 +0200

    add FluentValidation to CustomerService

--- src/application/TeslaCarSharing.Application/Services/CustomerService.cs
+++ src/application/TeslaCarSharing.Application/Services/CustomerService.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using System.ComponentModel.DataAnnotations;
 using TeslaCarSharing.Application.Contracts.Application;
 using TeslaCarSharing.Application.Contracts.Infrastructure;
 using TeslaCarSharing.Application.DTOs.Customer;
 using TeslaCarSharing.Core;
+using FluentValidation;
 
 namespace TeslaCarSharing.Application.Services;
 
@@ -10,15 +12,22 @@ public class CustomerService : ICustomerService
 {
     private readonly ICustomerRepository _repository;
     private readonly IMapper _mapper;
+    private readonly IValidator<CustomerDto> _validator;
 
-    public CustomerService(ICustomerRepository repository, IMapper mapper)
+    public CustomerService(ICustomerRepository repository, IMapper mapper, IValidator<CustomerDto> validator)
     {
         _repository = repository;
         _mapper = mapper;
+        _validator = validator;
     }
 
     public async Task<CustomerDto> Add(CustomerDto customerDto)
     {
+        var validationResult = await _validator.ValidateAsync(customerDto);
+        if (!validationResult.IsValid)
+        {
+            throw new FluentValidation.ValidationException(validationResult.Errors);
+        }
         var customer = _mapper.Map<Customer>(customerDto);
         var addedCustomer = await _repository.Add(customer);
         return _mapper.Map<CustomerDto>(addedCustomer);
@@ -47,6 +56,11 @@ public class CustomerService : ICustomerService
 
     public async Task Update(CustomerDto customerDto)
     {
+        var validationResult = await _validator.ValidateAsync(customerDto);
+        if (!validationResult.IsValid)
+        {
+            throw new FluentValidation.ValidationException(validationResult.Errors);
+        }
         var customer = _mapper.Map<Customer>(customerDto);
         await _repository.Update(customer);
     }
