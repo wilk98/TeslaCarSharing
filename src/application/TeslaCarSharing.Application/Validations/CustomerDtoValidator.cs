e006c702fd35e2c15e8505abc3135016baa72910
wilk98 <maciekwilk09@gmail.com>
2023-05-03 17:43:51 +0200

    add CustomerDtoValidator

---
+++ src/application/TeslaCarSharing.Application/Validations/CustomerDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TeslaCarSharing.Application.DTOs.Customer;
+
+namespace TeslaCarSharing.Application.Validations;
+
+public class CustomerDtoValidator : AbstractValidator<CustomerDto>
+{
+    public CustomerDtoValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.");
+        RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required.");
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Email is not valid.");
+        RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number is required.");
+    }
+}
