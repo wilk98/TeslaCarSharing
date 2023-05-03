31240db3e95ba5b244c24227e6d8623b5c7532b4
wilk98 <maciekwilk09@gmail.com>
2023-05-03 19:37:21 +0200

    delete CarPrices class

--- src/core/TeslaCarSharing.Core/CarPrices.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeslaCarSharing.Core;
-
-public static class CarPrices
-{
-    public const decimal ModelSPricePerDay = 200;
-    public const decimal Model3PricePerDay = 150;
-    public const decimal ModelXPricePerDay = 250;
-    public const decimal ModelYPricePerDay = 180;
-}
