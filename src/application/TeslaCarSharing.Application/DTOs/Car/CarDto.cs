e0aebd6ef12f0623688a2585bd34b759a210680a
wilk98 <maciekwilk09@gmail.com>
2023-05-03 04:03:39 +0200

    change CarDto to display string with Location and Model

--- src/application/TeslaCarSharing.Application/DTOs/Car/CarDto.cs
+++ src/application/TeslaCarSharing.Application/DTOs/Car/CarDto.cs
@@ -6,10 +6,10 @@ public class CarDto
 {
     public int Id { get; set; }
     public string RegistrationNumber { get; set; }
-    public TeslaModel Model { get; set; }
+    public string Model { get; set; }
     public string Color { get; set; }
     public int Year { get; set; }
-    public Location Location { get; set; }
+    public string Location { get; set; }
     public decimal PricePerDay { get; set; }
 
 }
